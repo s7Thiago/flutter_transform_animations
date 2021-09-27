9e55aa88ac6b946c4f8a655e9d53569528cd949d
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-27 20:16:28 -0300

    Construido base da lista

---
+++ lib/pages/list/model/character.dart
@@ -0,0 +1,256 @@
+class Character {
+  final String avatar;
+  final String title;
+  final String description;
+  final int color;
+
+  Character({
+    required this.avatar,
+    required this.title,
+    required this.description,
+    required this.color,
+  });
+}
+
+final List<Character> characters = [
+  Character(
+    title: 'Android 13',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155228-untitled_1.png',
+    color: 0xFFE83835,
+  ),
+  Character(
+    title: 'Android 14',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538740-a_14.jpg',
+    color: 0xFF11A01D,
+  ),
+  Character(
+    title: 'Android 15',
+    description:
+        'Twin brother of Android 18 (technically Cyborgs in this case) Android 17 was created by Dr. Gero, but rebelled and killed him. 17 is absorbed by Cell and dies with him, but is later wished back to life with Cell&#039;s victims. He becomes a Park Ranger and starts a family.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538746-a15.png',
+    color: 0xFAB1C308,
+  ),
+  Character(
+    title: 'Android 16',
+    description:
+        'Android 16 is an android created by Dr. Gero to kill Goku.  He was considered a failure and remained deactivated until Android 17 killed Gero and activated him.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/34651/1224189-016.jpg',
+    color: 0xFF6E0FAD,
+  ),
+  Character(
+    title: 'Android 17',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/5457/330298-android172.jpg',
+    color: 0xFF0BAFE0,
+  ),
+  Character(
+    title: 'Android 18',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155589-untitled_1.png',
+    color: 0xFF2CC7A5,
+  ),
+  Character(
+    title: 'Babidi',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155211-untitled_1.png',
+    color: 0xFFB80C84,
+  ),
+  Character(
+    title: 'Badrock',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/31/11745-bardock.jpg',
+    color: 0xFFE0740E,
+  ),
+  Character(
+    title: 'Beerus',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/16/164924/2418767-bills.png',
+    color: 0xFF0D9ADB,
+  ),
+  Character(
+    title: 'Bido',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/33873/1679705-bido.png',
+    color: 0xFF0B2EC8,
+  ),
+  Character(
+    title: 'Android 13',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155228-untitled_1.png',
+    color: 0xFFE83835,
+  ),
+  Character(
+    title: 'Android 14',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538740-a_14.jpg',
+    color: 0xFF11A01D,
+  ),
+  Character(
+    title: 'Android 15',
+    description:
+        'Twin brother of Android 18 (technically Cyborgs in this case) Android 17 was created by Dr. Gero, but rebelled and killed him. 17 is absorbed by Cell and dies with him, but is later wished back to life with Cell&#039;s victims. He becomes a Park Ranger and starts a family.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538746-a15.png',
+    color: 0xFAB1C308,
+  ),
+  Character(
+    title: 'Android 16',
+    description:
+        'Android 16 is an android created by Dr. Gero to kill Goku.  He was considered a failure and remained deactivated until Android 17 killed Gero and activated him.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/34651/1224189-016.jpg',
+    color: 0xFF6E0FAD,
+  ),
+  Character(
+    title: 'Android 17',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/5457/330298-android172.jpg',
+    color: 0xFF0BAFE0,
+  ),
+  Character(
+    title: 'Android 18',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155589-untitled_1.png',
+    color: 0xFF2CC7A5,
+  ),
+  Character(
+    title: 'Babidi',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155211-untitled_1.png',
+    color: 0xFFB80C84,
+  ),
+  Character(
+    title: 'Badrock',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/31/11745-bardock.jpg',
+    color: 0xFFE0740E,
+  ),
+  Character(
+    title: 'Beerus',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/16/164924/2418767-bills.png',
+    color: 0xFF0D9ADB,
+  ),
+  Character(
+    title: 'Bido',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/33873/1679705-bido.png',
+    color: 0xFF0B2EC8,
+  ),
+  Character(
+    title: 'Android 13',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155228-untitled_1.png',
+    color: 0xFFE83835,
+  ),
+  Character(
+    title: 'Android 14',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538740-a_14.jpg',
+    color: 0xFF11A01D,
+  ),
+  Character(
+    title: 'Android 15',
+    description:
+        'Twin brother of Android 18 (technically Cyborgs in this case) Android 17 was created by Dr. Gero, but rebelled and killed him. 17 is absorbed by Cell and dies with him, but is later wished back to life with Cell&#039;s victims. He becomes a Park Ranger and starts a family.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/8/89459/1538746-a15.png',
+    color: 0xFAB1C308,
+  ),
+  Character(
+    title: 'Android 16',
+    description:
+        'Android 16 is an android created by Dr. Gero to kill Goku.  He was considered a failure and remained deactivated until Android 17 killed Gero and activated him.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/34651/1224189-016.jpg',
+    color: 0xFF6E0FAD,
+  ),
+  Character(
+    title: 'Android 17',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/5457/330298-android172.jpg',
+    color: 0xFF0BAFE0,
+  ),
+  Character(
+    title: 'Android 18',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155589-untitled_1.png',
+    color: 0xFF2CC7A5,
+  ),
+  Character(
+    title: 'Babidi',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/39164/1155211-untitled_1.png',
+    color: 0xFFB80C84,
+  ),
+  Character(
+    title: 'Badrock',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/0/31/11745-bardock.jpg',
+    color: 0xFFE0740E,
+  ),
+  Character(
+    title: 'Beerus',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/16/164924/2418767-bills.png',
+    color: 0xFF0D9ADB,
+  ),
+  Character(
+    title: 'Bido',
+    description:
+        'Finished long after Dr. Gero&#039;s death by his self-aware supercomputer, Android 13 is specifically designed to destroy Goku. He can obtain a greater form once absorbing components from #14 and #15.',
+    avatar:
+        'https://www.giantbomb.com/a/uploads/square_small/3/33873/1679705-bido.png',
+    color: 0xFF0B2EC8,
+  ),
+];
--- lib/pages/list/shrink_top_list.dart
+++ lib/pages/list/shrink_top_list.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:transform_example/pages/list/model/character.dart';
 
-class ShrinkTopList extends StatelessWidget {
+const itemSize = 150.0;
+
+class ShrinkTopList extends StatefulWidget {
   const ShrinkTopList({Key? key}) : super(key: key);
+
+  @override
+  State<ShrinkTopList> createState() => _ShrinkTopListState();
+}
+
+class _ShrinkTopListState extends State<ShrinkTopList> {
+  final scrollController = ScrollController();
+
+  void onListen() {}
+
+  @override
+  void initState() {
+    scrollController.addListener(onListen);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(onListen);
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +34,43 @@ class ShrinkTopList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Shrink top list'),
       ),
-      body: Container(),
+      body: CustomScrollView(
+        controller: scrollController,
+        slivers: [
+          SliverList(
+            // ! Este delegate será usando porque serve para construir os itens dinamicamente
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final character = characters[index];
+                return Card(
+                  color: Color(character.color),
+                  child: SizedBox(
+                    height: itemSize,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              character.title,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Image.network(character.avatar),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              childCount: characters.length,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
