9d3fa98435ab3faee6597bda5ff2f1ac858b6785
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-27 20:58:46 -0300

    Animação com placeholder no topo para simular um header

--- lib/pages/list/shrink_top_list.dart
+++ lib/pages/list/shrink_top_list.dart
@@ -41,42 +41,66 @@ class _ShrinkTopListState extends State<ShrinkTopList> {
         child: CustomScrollView(
           controller: scrollController,
           slivers: [
+            const SliverToBoxAdapter(
+              child: Placeholder(
+                fallbackHeight: 100.0,
+              ),
+            ),
             SliverList(
               // ! Este delegate será usando porque serve para construir os itens dinamicamente
               delegate: SliverChildBuilderDelegate(
                 (context, index) {
                   final character = characters[index];
 
-                  final itemPositionOffset = index * itemSize;
+                  final itemPositionOffset = index * itemSize / 2;
                   final difference =
                       scrollController.offset - itemPositionOffset;
-                  final percent = 1 - (difference / itemSize);
+                  final percent = 1 - (difference / (itemSize / 2));
                   double opacity = percent.clamp(0.0, 1.0);
+                  double scale = percent.clamp(0.0, 1.0);
 
-                  return Opacity(
-                    opacity: opacity,
-                    child: Transform(
-                      transform: Matrix4.identity()..scale(percent, 1.0),
-                      child: Card(
-                        color: Color(character.color),
-                        child: SizedBox(
-                          height: itemSize,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: Text(
-                                    character.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 25,
+                  return Align(
+                    heightFactor: .5,
+                    child: Opacity(
+                      opacity: opacity,
+                      child: Transform(
+                        alignment: Alignment.topCenter,
+                        transform: Matrix4.identity()
+                          // ..setEntry(3, 2, 0.001)
+                          ..scale(scale, 1),
+                        child: Card(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20.0),
+                            topRight: Radius.circular(20.0),
+                          )),
+                          color: Color(character.color),
+                          child: SizedBox(
+                            height: itemSize,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Text(
+                                      character.title,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Image.network(character.avatar),
-                            ],
+                                ClipRRect(
+                                  borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(
+                                      20.0,
+                                    ),
+                                  ),
+                                  child: Image.network(character.avatar),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
