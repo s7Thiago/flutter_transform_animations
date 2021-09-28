2409d350379497135566eb9771ca85a62c32aaa6
Thiago Silva <thyagosousasilva@gmail.com>
2021-09-28 15:33:48 -0300

    Adicionando navegação automática para a página inicial do exemplo de custom transitions

--- lib/pages/transitions/example1/transition1_example_splash_screen.dart
+++ lib/pages/transitions/example1/transition1_example_splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transform_example/pages/transitions/example1/transitions1_page.dart';
 
 class Transition1ExampleSplashScreen extends StatefulWidget {
   const Transition1ExampleSplashScreen({Key? key}) : super(key: key);
@@ -12,6 +13,13 @@ class _Transition1ExampleSplashScreenState
     extends State<Transition1ExampleSplashScreen> {
   @override
   void initState() {
+    Future.delayed(Duration(seconds: 3)).then(
+      (value) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const Transitions1Page(),
+        ),
+      ),
+    );
     super.initState();
   }
 
